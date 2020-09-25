ac448b10f4f03969e6b97d5bc45f9ed673f38888
RishabhShekhar <rishabhshekhar04@gmail.com>
2020-09-25 22:48:25 +0530

    Setting Up

---
+++ Bank Management System/bank_management.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"1-> Open Account \n";
+    cout<<"2-> Deposit Money \n";
+    cout<<"3-> Withdraw Money \n";
+    cout<<"4-> Display Account \n";
+    cout<<"5-> Exit \n";
+    
+    return 0;
+}
