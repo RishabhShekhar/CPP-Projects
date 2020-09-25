d77d165c352cb5bbcedb08292c99296d41a4c2a0
RishabhShekhar <rishabhshekhar04@gmail.com>
2020-09-25 23:07:46 +0530

    Open And Deposit in account

--- Bank Management System/bank_management.cpp
+++ Bank Management System/bank_management.cpp
@@ -1,5 +1,37 @@
 #include<iostream>
 using namespace std;
+
+class bank{
+    char name[100],add[100],y;
+    int balance;
+    public:
+        void open_account();
+        void deposit();
+        void withdraw();
+        void display();
+};
+
+void bank :: open_account(){
+    cout<<"Enter your full name : ";
+    cin.ignore();
+    cin.getline(name,100);
+    cout<<"Enter your address : ";
+    cin.ignore();
+    cin.getline(add,100);
+    cout<<"What type of account you want to open Saving (S) or Current (C) : ";
+    cin>>y;
+    cout<<"Enter amount for deposit : ";
+    cin>>balance;
+    cout<<"Your account is created";
+}
+
+void bank :: deposit(){
+    int a;
+    cout<<"Enter amout to be deposited : ";
+    cin>>a;
+    balance+=a;
+    cout<<"Total balance : \t"<<balance;
+}
 int main(){
     int a;
     cout<<"1-> Open Account \n";
@@ -7,6 +39,6 @@ int main(){
     cout<<"3-> Withdraw Money \n";
     cout<<"4-> Display Account \n";
     cout<<"5-> Exit \n";
-    
+
     return 0;
 }
